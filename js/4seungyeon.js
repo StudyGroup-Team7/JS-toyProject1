614f780790578aff6ea087b32ecf8a54558413a2
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-01-17 03:12:18 +0900

    feat: Implement resetButtonEl function

--- js/4seungyeon.js
+++ js/4seungyeon.js
@@ -1,13 +1,25 @@
 const secondEl = document.querySelector('.second')
 const milliSecondEl = document.querySelector('.milli-second')
 const startButtonEl = document.querySelector('.btn-start')
+const resetButtonEl = document.querySelector('.btn-reset')
+
+let startIntervalId
 
 let sec = '00'
 let msec = '00'
 
 // Events
 startButtonEl.addEventListener('click', function () {
-  setInterval(startTimer, 10)
+  startIntervalId = setInterval(startTimer, 10)
+})
+
+resetButtonEl.addEventListener('click', function () {
+  clearInterval(startIntervalId)
+
+  sec = '00'
+  msec = '00'
+  secondEl.textContent = sec
+  milliSecondEl.textContent = msec
 })
 
 window.addEventListener('DOMContentLoaded', function () {
