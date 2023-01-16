69ee6bf873370b7eb55e1dc7696377a407d27142
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-01-17 03:04:58 +0900

    feat: Implement startButtonEl function

--- js/4seungyeon.js
+++ js/4seungyeon.js
@@ -1,10 +1,40 @@
 const secondEl = document.querySelector('.second')
 const milliSecondEl = document.querySelector('.milli-second')
+const startButtonEl = document.querySelector('.btn-start')
 
 let sec = '00'
 let msec = '00'
+
+// Events
+startButtonEl.addEventListener('click', function () {
+  setInterval(startTimer, 10)
+})
 
 window.addEventListener('DOMContentLoaded', function () {
   secondEl.innerHTML = sec
   milliSecondEl.innerHTML = msec
 })
+
+// Functions
+function startTimer() {
+  msec++
+
+  if (msec <= 9) {
+    milliSecondEl.innerHTML = `0${msec}`
+  }
+
+  if (msec > 9) {
+    milliSecondEl.innerHTML = msec
+  }
+
+  if (msec > 99) {
+    sec++
+    secondEl.innerHTML = `0${sec}`
+    msec = 0
+    milliSecondEl.innerHTML = `0${msec}`
+  }
+
+  if (sec > 9) {
+    secondEl.innerHTML = sec
+  }
+}
