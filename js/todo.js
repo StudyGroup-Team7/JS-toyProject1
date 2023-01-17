97f22b81a143f70ed52daa54dd42c909741ce484
Hana-Korea <117172983+Hana-Korea@users.noreply.github.com>
2023-01-17 14:36:53 +0900

    feat: Add to-do list

--- js/todo.js
+++ js/todo.js
@@ -3,7 +3,7 @@ const todoTxt = document.querySelector(".todo-txt");
 const todoBtn = document.querySelector("todo-btn");
 const todoList = document.querySelector(".todo-list");
 
-const toDos = [];
+let toDos = [];
 
 function saveTodos() {
   localStorage.setItem("toDos", toDos);
@@ -12,21 +12,25 @@ function todoSubmit(event) {
   event.preventDefault();
   const task = todoTxt.value;
   todoTxt.value = "";
-  toDos.push(task);
-  paintTodo(task);
-  saveTodos(task);
+  const taskObj = {
+    task,
+    id: Date.now(),
+  };
+  toDos.push(taskObj);
+  paintTodo(taskObj);
+  saveTodos();
   localStorage.setItem("toDos", JSON.stringify(toDos));
 }
 
 todoForm.addEventListener("submit", todoSubmit);
 
 // 새로운 할 일 추가 & 삭제 버튼 구현
-function paintTodo(task) {
+function paintTodo(taskObj) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   const button = document.createElement("button");
   button.addEventListener("click", deleteTodo);
-  span.textContent = task;
+  span.textContent = taskObj.task;
   button.textContent = "❌";
   li.append(span);
   li.append(button);
@@ -37,10 +41,14 @@ function paintTodo(task) {
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveTodos();
 }
 
-const savedTodos = JSON.parse(localStorage.getItem("todos"));
-console.log(savedTodos);
-// if (saveTodos !== null) {
-//   saveTodos.forEach((item) => console.log(`${item}`));
-// }
+const getTodos = localStorage.getItem("toDos");
+
+if (getTodos !== null) {
+  const parsedTodos = JSON.parse(getTodos);
+  toDos = parsedTodos;
+  parsedTodos.forEach(paintTodo);
+}
